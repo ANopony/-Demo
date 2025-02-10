c65fe518cbe7eb7c4de7401bcc72b6859cfee12e
zhoulxg <zhoulxg@digitalchina.com>
2025-02-10 14:56:07 +0800

    Init

---
+++ chat.cs
@@ -0,0 +1,5 @@
+// 访问API实现chat功能
+
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
---
+++ main.cs
@@ -0,0 +1,10 @@
+// 从hello world开始
+using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
