5d66a8afbd1042a307b67b43c4bdd624ec245af2
zhoulxg <zhoulxg@digitalchina.com>
2025-02-10 15:06:09 +0800

    chat

--- chat.cs
+++ chat.cs
@@ -3,3 +3,17 @@
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+
+class Chat
+{
+    static async Task Main()
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync("https://api.github.com/repos/dotnet/corefx/issues");
+        var stream = await response.Content.ReadAsStreamAsync();
+        using (var reader = new StreamReader(stream))
+        {
+            System.Console.WriteLine(await reader.ReadToEndAsync());
+        }
+    }
+}
